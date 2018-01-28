d43728f5dfb4d1f064d1fd079e4b1f223f689526
Chuy <chuy023@gmail.com>
2018-01-27 18:13:24 -0600

    added components

--- shopping-center/src/app/app.module.ts
+++ shopping-center/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CategoryCardsComponent } from './category-cards/category-cards.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent,
+    CategoryCardsComponent
   ],
   imports: [
     BrowserModule
---
+++ shopping-center/src/app/category-cards/category-cards.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CategoryCardsComponent } from './category-cards.component';
+
+describe('CategoryCardsComponent', () => {
+  let component: CategoryCardsComponent;
+  let fixture: ComponentFixture<CategoryCardsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CategoryCardsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryCardsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
